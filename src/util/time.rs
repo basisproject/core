c7b2193147f2474416bf250de10ce78153d21174
Andrew Danger Lyon <orthecreedence@gmail.com>
2020-06-13 08:23:50 -0700

    adding unused is_leap_year time util fn

--- src/util/time.rs
+++ src/util/time.rs
@@ -1,5 +1,12 @@
 use chrono::{DateTime, Utc};
 
+/// Determine if a year (eg `2017`) is a leap year.
+#[allow(dead_code)]
+pub fn is_leap_year(year: i64) -> bool {
+    ((year % 4) == 0 && (year % 100) != 0) || (year % 400) == 0
+}
+
+/// Get the current time. Mainly for testing.
 #[allow(dead_code)]
 pub fn now() -> DateTime<Utc> {
     Utc::now()
