ceea73f91ee9c9818ad5285687da15e6221be364
Andrew Danger Lyon <orthecreedence@gmail.com>
2020-06-30 23:43:12 -0700

    commitment model: adding `validate_event() method which checks the commitment to make sure it is for that event being processed. was going to use this for transfer validation, but realized that that is better accomplished by counterparty signing, which is probably not going to live in the same level as the core. meaning, might be unused for now. oh well, keeping it in for now.

--- src/models/commitment.rs
+++ src/models/commitment.rs
@@ -9,8 +9,11 @@
 //! [Event]: ../event/struct.Event.html
 
 use crate::{
+    error::{Error, Result},
+    costs::Costs,
     models::{
         agreement::AgreementID,
+        event::Event,
         lib::agent::AgentID,
         process::ProcessID,
         resource::ResourceID,
@@ -28,8 +31,39 @@ basis_model! {
     /// [vfcomm]: https://valueflo.ws/introduction/flows.html#commitment
     pub struct Commitment {
         id: <<CommitmentID>>,
+        /// The commitments's core VF type
         inner: vf::Commitment<AgreementID, AgreementID, AgentID, (), ProcessID, AgentID, (), ResourceSpecID, ResourceID>,
+        /// The amount of costs committed to be moved. One could think of this
+        /// somewhat like a negotiated price in the current system.
+        move_costs: Option<Costs>,
     }
     CommitmentBuilder
+}
+
+impl Commitment {
+    /// Given an event, make sure the event either
+    ///
+    /// - matches the current commitment (meaning the companies, processes, and
+    /// resources in the event are the same as they are in the commitment)
+    /// - does not require a commitment (ie, an internal transfer or something)
+    pub fn validate_event(&self, event: &Event) -> Result<()> {
+        match event.inner().action() {
+            // inter-organizational actions actions need validation
+            vf::Action::DeliverService | vf::Action::Transfer | vf::Action::TransferAllRights | vf::Action::TransferCustody => {}
+            // intra-organizational actions do not need validation
+            _ => { return Ok(()); }
+        }
+        if self.inner().provider() != event.inner().provider() ||
+            self.inner().receiver() != event.inner().receiver() ||
+            self.inner().input_of() != event.inner().input_of() ||
+            self.inner().output_of() != event.inner().output_of() ||
+            self.inner().resource_inventoried_as() != event.inner().resource_inventoried_as() ||
+            self.inner().resource_quantity() != event.inner().resource_quantity() ||
+            self.move_costs() != event.move_costs()
+        {
+            Err(Error::CommitmentInvalid)?;
+        }
+        Ok(())
+    }
 }
 
