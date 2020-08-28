eafdf60db7ea96bf52096ec47265f8e527d30785
Andrew Danger Lyon <orthecreedence@gmail.com>
2020-08-28 07:37:09 -0700

    apparently this is already done (converting user deleted error to object deleted), just needed to remove the old error type..

--- src/error.rs
+++ src/error.rs
@@ -67,9 +67,6 @@ pub enum Error {
     /// match expectation.
     #[error("Op does not match expectation")]
     OpMismatch,
-    /// When we try to perform an operation on a deleted user
-    #[error("operation on deleted user")]
-    UserIsDeleted,
     /// When we try to convert an AgentID to another ID type but it fails (like
     /// `let company_id: CompanyID = AgentID::UserID(user_id).try_from()?;`).
     #[error("AgentID is the wrong type")]
