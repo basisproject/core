14ee1503f1b2a78f0c9bc1f67aa626698f64bc99
Andrew Danger Lyon <orthecreedence@gmail.com>
2020-09-14 20:42:40 -0700

    adding currency model/transaction w/ tests

--- src/models/currency.rs
+++ src/models/currency.rs
@@ -1,10 +1,12 @@
 //! The currency module holds the Currency model, used to track various currency
 //! types for purposes of [banking].
 //!
+//! Note that currencies require global systemic management.
+//!
 //! [banking]: https://basisproject.gitlab.io/public/paper#chapter-6-banking
 
 basis_model! {
-    /// The currency model allows the backing system to track various currencies
+    /// The currency model allows the banking system to track various currencies
     /// as they move through the system, which ultimately allows an accurate
     /// conversion between the internal credits and external monetary systems.
     pub struct Currency {
@@ -12,7 +14,7 @@ basis_model! {
         /// The name of the currency, probably some ISO value.
         name: String,
         /// How many decimal places this currency uses.
-        decimal_scale: u32,
+        decimal_places: u8,
     }
     CurrencyBuilder
 }
